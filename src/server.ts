458f842959075e50e2ccf907742cb3bca439f1ce
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2023-12-20 23:01:35 +0100

    feat: add main route response

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import Express from "express";
+import Express, { Request, Response } from "express";
 import helmet from "helmet";
 import userRoutes from "./routes/user.routes";
 import movieRoutes from "./routes/movie.routes";
@@ -15,5 +15,9 @@ app.use(Express.json());
 app.use("/users", jwtCheckMiddleware, userRoutes);
 app.use("/movies", jwtCheckMiddleware, movieRoutes);
 app.use("/genres", jwtCheckMiddleware, genreRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json("Welcome to the Moviehub API");
+});
 
 export default app;
